daf3629de9897ca1e7c631a84691304ff58be12b
tweed1 <148729136+tweed1@users.noreply.github.com>
2025-10-14 12:49:52 -0700

    Completed 01-flexbox and updated 02-palindrome
    palindrome is working but i would like to refine and mess with function definition vs expression

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,18 @@
 const elem = document.querySelector('input');
-
+const outputElem = document.querySelector('section div div');
 elem.addEventListener('input', handleInput);
+
+function handleInput(event) {
+    const value = event.target.value;
+    console.log("current value: ", value);
+    const str = value.toString();
+    const reversed = str.split('').reverse().join('');
+    const isPalindrome = (str === reversed);
+
+    if (isPalindrome) {
+        outputElem.textContent = `${value} is a palindrome`;
+        outputElem.style.color = `green`;
+    }
+}
+
+
