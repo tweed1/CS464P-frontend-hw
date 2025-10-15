53a7e324447b30aa65f867d9c7b2e19c1d2d7d0f
tweed1 <148729136+tweed1@users.noreply.github.com>
2025-10-14 23:11:35 -0700

    update with not palindrome and function expression

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,18 +1,21 @@
-const elem = document.querySelector('input');
-const outputElem = document.querySelector('section div div');
-elem.addEventListener('input', handleInput);
+const elem = document.querySelector("input");
+const outputElem = document.querySelector("section div div");
 
-function handleInput(event) {
-    const value = event.target.value;
-    console.log("current value: ", value);
-    const str = value.toString();
-    const reversed = str.split('').reverse().join('');
-    const isPalindrome = (str === reversed);
+const handleInput = function handleInputFunction(event) {
+  const value = event.target.value;
+  console.log("current value: ", value);
+  const str = value.toString();
+  const reversed = str.split("").reverse().join("");
+  const isPalindrome = str === reversed;
 
-    if (isPalindrome) {
-        outputElem.textContent = `${value} is a palindrome`;
-        outputElem.style.color = `green`;
-    }
-}
+  if (isPalindrome) {
+    outputElem.textContent = `${value} is a palindrome`;
+    outputElem.style.color = `green`;
+  }
+  else {
+    outputElem.textContent = `${value} is NOT a palindrome`;
+    outputElem.style.color = `red`;
+  }
+};
 
-
+elem.addEventListener("input", handleInput);
