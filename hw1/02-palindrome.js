381836a69d3294da4a315908b343e36f0b55ddb1
tweed1 <148729136+tweed1@users.noreply.github.com>
2025-10-15 13:19:40 -0700

    styling palindrome + adding more input validation

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -2,13 +2,22 @@ const elem = document.querySelector("input");
 const outputElem = document.querySelector("section div div");
 
 const handleInput = function handleInputFunction(event) {
-  const value = event.target.value;
-  console.log("current value: ", value);
-  const str = value.toString();
-  const reversed = str.split("").reverse().join("");
-  const isPalindrome = str === reversed;
+  
+    const value = event.target.value;
+  //console.log("current value: ", value);
+  //const str_value = value.toString(); // is this check important?
+  const reversed = value.split("").reverse().join("");
+  const isPalindrome = (value === reversed);
 
-  if (isPalindrome) {
+  if (value === "") {
+    outputElem.textContent = `Please enter a number`;
+    outputElem.style.color = `purple`;
+  }
+  else if (value < 0) {
+    outputElem.textContent = `${value} is not a valid entry`;
+    outputElem.style.color = `orange`;
+  }
+  else if (isPalindrome) {
     outputElem.textContent = `${value} is a palindrome`;
     outputElem.style.color = `green`;
   }
