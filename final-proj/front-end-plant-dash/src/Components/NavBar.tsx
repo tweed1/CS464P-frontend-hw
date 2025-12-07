98e28c558268a0b78bc6abfb3fe1ce8e90d9a9f9
tweed1 <148729136+tweed1@users.noreply.github.com>
2025-12-07 12:49:30 -0800

    navbar refinement

--- final-proj/front-end-plant-dash/src/Components/NavBar.tsx
+++ final-proj/front-end-plant-dash/src/Components/NavBar.tsx
@@ -1,33 +1,3 @@
-/* Used a bootstrap template for the navbar */
-
-/* import { Link } from "react-router";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
-function NavBar() {
-	return (
-		<Navbar expand="lg" className="bg-body-tertiary">
-			<Container>
-				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#link">Link</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
-}
-
-export default NavBar;
- */
-
-/* Used a bootstrap template for the navbar */
-
 import { Link } from "react-router";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
@@ -35,43 +5,48 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Stack from "react-bootstrap/Stack";
 import Image from "react-bootstrap/Image";
-import '../custom.css';
+import "../custom.css";
 import flower from "../images/half-main-flower.png";
 
 const Navbar = () => {
 	return (
-        <>
-		<nav className="navbar navbar-expand-lg navbar-light navbar-brand custom-primary bg-gradient py-0 mx-0 ">
-		
-				<div className="container-fluid d-flex align-items-center justify-content-between " id="navbarSupportedContent">
-                    <Link to="/" className="navbar-brand me-3 text-white my-agu">
-                    PLANTIFY</Link>
-					<ul className="navbar-nav flex-row mx-auto mb-2 mb-lg-0 ">
+		<>
+			<nav className="navbar navbar-expand-lg navbar-light navbar-brand custom-primary bg-gradient py-0 mx-0 ">
+				<div
+					className="container-fluid d-flex align-items-center justify-content-between"
+					id="navbarSupportedContent">
+					<Link
+						to="/"
+						className="navbar-brand me-3 text-white my-agu fs-4">
+						PLANTIFY
+					</Link>
+					<ul className="navbar-nav flex-row mb-2 mb-lg-0 align-items-center justify-content-center">
 						<li className="nav-item px-2">
-							<Link to="/" className="nav-link active text-white my-ultra">
+							<Link
+								to="/"
+								className="nav-link active text-white my-ultra nav-pill">
 								{" "}
 								Home
 							</Link>
 						</li>
-						<li className="nav- px-2">
-							<Link to="/search" className="nav-link active text-white my-ultra">
+						<li className="px-2">
+							<Link
+								to="/search"
+								className="nav-link active text-white my-ultra nav-pill">
 								Search
 							</Link>
 						</li>
-						<li className="nav-item px-2">
-							<Link to="/zonemap" className="nav-link active text-white my-ultra">
-								Zone Map
+						{/* 
+						<li className="px-2">
+							<Link to="/mystery-plant" className="nav-link active text-white my-ultra nav-pill">
+								Mystery
 							</Link>
-						</li>
+						</li> */}
 					</ul>
-                    <Button className="btn-secondary bd rounded-pill py-1 my-ultra">
-                    mystery
-                    </Button>
 				</div>
-			
-		</nav>
-        {/* Flower attached to navbar */}
-        <Container fluid className="mx-0 px-0">
+			</nav>
+			{/* Flower attached to navbar */}
+			<Container fluid className="mx-0 px-0">
 				<Row>
 					<Col></Col>
 					<Col></Col>
@@ -85,7 +60,7 @@ const Navbar = () => {
 					</Col>
 				</Row>
 			</Container>
-        </>
+		</>
 	);
 };
 
